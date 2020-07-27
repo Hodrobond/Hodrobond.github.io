84047211cbd77665a3c5daa5b18ba5436219ed70
Adam Kazberuk <hodrobond@gmail.com>
2020-07-26 19:32:05 -0700

    Test

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,6 +5,7 @@ const preferDefault = m => m && m.default || m
 
 
 exports.components = {
+  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/hodrobond/git/gatsby-starter-blog/.cache/dev-404-page.js"))),
   "component---src-pages-404-js": hot(preferDefault(require("/Users/hodrobond/git/gatsby-starter-blog/src/pages/404.js"))),
   "component---src-pages-index-js": hot(preferDefault(require("/Users/hodrobond/git/gatsby-starter-blog/src/pages/index.js"))),
   "component---src-pages-test-js": hot(preferDefault(require("/Users/hodrobond/git/gatsby-starter-blog/src/pages/test.js"))),
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,32 +1,47 @@
-import React from "react"
+import React, { useState, useMemo } from "react"
 import { Link, graphql } from "gatsby"
 import BlogPostCard from '@hodrobond/ui-blogpostcard';
-
 import Bio from "@hodrobond/ui-developercard"
+import Themes from '@hodrobond/ui-themes';
+import ThemeWrapper from '@hodrobond/ui-themewrapper';
+import ThemePicker from '@hodrobond/ui-themepicker';
+
+import Sidebar from '../components/sidebar';
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import { rhythm } from "../utils/typography"
 
-import Sidebar from '../components/sidebar';
-
 const BlogIndex = ({ data, location }) => {
-  const siteTitle = data.site.siteMetadata.title
-  const posts = data.allMarkdownRemark.edges
+  const [theme, updateTheme] = useState('aqua');
+  const handleChange = useMemo(() => (event) => {
+    const { target: { value } = {} } = event || {};
+    updateTheme(value);
+  }, []);
+  const siteTitle = data.site.siteMetadata.title;
+  const posts = data.allMarkdownRemark.edges;
   return (
     <Layout location={location} title={siteTitle}>
-      <SEO title="All posts" />
-      <Bio />
-      <Sidebar />
-      {posts.map(({ node }) => (
-        <BlogPostCard
-          title={{
-            text: node.frontmatter.title || node.fields.slug,
-            link: node.fields.slug,
-          }}
-          date={new Date(node.frontmatter.date)}
-          description={node.frontmatter.description || node.excerpt}
-        />
-    ))}
+      <ThemeWrapper
+        s={Themes[theme]}
+      >
+        <SEO title="All posts" />
+        <Bio />
+        <Sidebar>
+          <ThemePicker
+            handleChange={handleChange}
+          />
+        </Sidebar>
+        {posts.map(({ node }) => (
+          <BlogPostCard
+            title={{
+              text: node.frontmatter.title || node.fields.slug,
+              link: node.fields.slug,
+            }}
+            date={new Date(node.frontmatter.date)}
+            description={node.frontmatter.description || node.excerpt}
+          />
+        ))}
+      </ThemeWrapper>
     </Layout>
   )
 }
